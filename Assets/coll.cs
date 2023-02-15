59c815905a3d5654504ebc6ceaf1581aeb36ed0c
Junseong <david991129@naver.com>
2023-02-15 19:08:48 +0900

    three

--- Assets/coll.cs
+++ Assets/coll.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class coll : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("yee");
@@ -11,6 +23,20 @@ public class coll : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("what?");
+        if(other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package picked up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package Delivered");
+            spriteRenderer.color = noPackageColor;
+            hasPackage = false;
+        }
+
     }
 }
